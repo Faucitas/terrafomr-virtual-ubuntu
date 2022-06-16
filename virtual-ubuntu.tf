46827747e8d50e0acbbd78a47c9e212d7797737a
Tommy Smith <tommy@tommyesmith.com>
2022-06-16 15:29:42 -0400

    crete secuity group

--- virtual-ubuntu.tf
+++ virtual-ubuntu.tf
@@ -11,3 +11,60 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+data "aws_ami" "ubuntu_desktop_ami" {
+    most_recent = true
+    owners = [ "aws-marketplace" ]
+
+    filter {
+      name = "image-id"
+      values = ["ami-05f4819e1de6aa2b8"]
+    }
+}
+
+# Configure security group
+resource "aws_security_group" "ubuntu_ami" {
+  name        = "ubuntu_desktop_live"
+  description = "Allow HTTP, RDP and SSH inbound traffic"
+
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "RDP"
+    from_port        = 3389
+    to_port          = 3389
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
