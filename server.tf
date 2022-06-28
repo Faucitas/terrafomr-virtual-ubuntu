10a4c0ff748a172a3d8fc142c471128255ba284b
Tommy Smith <tommy@tommyesmith.com>
2022-06-28 12:53:32 -0400

    Add elastic ip address

--- server.tf
+++ server.tf
@@ -1,8 +1,8 @@
 
 # Configure security group
 resource "aws_security_group" "ubuntu_ami_sg" {
-  name        = "ubuntu_desktop_live"
-  description = "Allow HTTP, RDP and SSH inbound traffic"
+  name        = "ec2_sg_group"
+  description = "Allow HTTP, HTTPS and SSH inbound traffic"
 
   ingress {
     description      = "HTTP"
@@ -28,14 +28,6 @@ resource "aws_security_group" "ubuntu_ami_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description      = "RDP"
-    from_port        = 3389
-    to_port          = 3389
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -48,10 +40,15 @@ resource "aws_instance" "ubuntu_ec2" {
   ami           = data.aws_ami.ubuntu_desktop_ami.id
   instance_type = var.instance_type
   security_groups = [aws_security_group.ubuntu_ami_sg.name]
-  key_name = "ubuntu-live"
-  user_data = "${file("./user-data.sh")}"
+  key_name = var.key_name
+  # user_data = "${file("./user-data.sh")}"1
+}
+
+resource "aws_eip" "server_ip" {
+  instance = aws_instance.ubuntu_ec2.id
+  vpc      = true
 }
 
 output "IP" {
-  value = "${aws_instance.ubuntu_ec2 .public_ip}"
+  value = "${aws_eip.server_ip.public_ip}"
 }
