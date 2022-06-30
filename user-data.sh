3894fa3d49818c0a84d7abf8c7f3c68157fe31ba
Tommy Smith <tommy@tommyesmith.com>
2022-06-30 17:16:37 -0400

    Add aws-cli installation to user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 apt update
+apt upgrade -y
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-# Get and install docker repository
+# Get and install docker and docker compose
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 
 apt install docker-ce -y
 
-# install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Add user to docker group
 usermod -aG docker ubuntu
+
+# Install aws-cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+apt install unzip
+unzip awscliv2.zip
+./aws/install
