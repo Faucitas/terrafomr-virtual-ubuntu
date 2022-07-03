5eeb9016145982c4eee104e73654fcef40ccfb60
Tommy Smith <tommy@tommyesmith.com>
2022-07-03 13:53:46 -0400

    Add default values for variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,32 @@
 variable "image_id" {
   type        = string
   description = "The AWS ami id for the base ec2 os"
+  default = "ami-0c24d345ea91339ee"
 }
 
 variable "instance_type" {
   type        = string
   description = "The name of the ec2 instance size"
+  default = "t2.micro"
 }
 
 variable "region" {
   type        = string
   description = "Region to deploy the server in"
+  default = "us-east-1"
 }
 
 variable "key_name" {
   type        = string
   description = "Key file to use for ssh"
+  default = "ubuntu-live"
 }
 
 variable "default_tags" {
   type = map(string)
   description = "Default tags for all provisioned infastructure"
+  default = {
+    project = "Django Blog"
+    costcenter = "Test Projects"
+  }
 }
